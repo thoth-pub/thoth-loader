d9f6afa264c57012f845565bfe642f1a78eaee83
Brendan OConnell <boconnell1@gmail.com>
2024-01-26 10:47:03 +0100

    fixed exception for failed update to Work, removed work_id from methods where not needed, rewrote contributor logic to prefer orcid_id over full_name as identifier for all_contributors if available

--- scieloloader.py
+++ scieloloader.py
@@ -28,18 +28,18 @@ class SciELOLoader(BookLoader):
                         self.thoth.update_work(existing_work)
                         logging.info(f"updated workId: {work_id}")
                     # if update fails, log the error and exit the import
-                    except Exception as e:
-                        logging.error(f"Failed to update work with id {work_id}, exception: {e}")
-                        return
+                    except ThothError as t:
+                        logging.error(f"Failed to update work with id {work_id}, exception: {t}")
+                        exit(1)
             # if work isn't found, create it
             except (IndexError, AttributeError, ThothError):
                 work_id = self.thoth.create_work(work)
                 logging.info(f"created workId: {work_id}")
             work = self.thoth.work_by_id(work_id)
-            self.create_publications(record, work, work_id)
-            self.create_contributors(record, work, work_id)
-            self.create_languages(record, work, work_id)
-            self.create_subjects(record, work, work_id)
+            self.create_publications(record, work)
+            self.create_contributors(record, work)
+            self.create_languages(record, work)
+            self.create_subjects(record, work)
 
     def get_work(self, record, imprint_id):
         """Returns a dictionary with all attributes of a 'work'
@@ -104,12 +104,12 @@ class SciELOLoader(BookLoader):
         }
         return work
 
-    def create_publications(self, record, work, work_id):
+    def create_publications(self, record, work):
         """Creates PDF, EPUB, and paperback publications associated with the current work
 
         record: current JSON record
 
-        work_id: previously obtained ID of the current work
+        work: Work from Thoth
         """
         books_url = record["books_url"]
         pdf_url = record["pdf_url"]
@@ -119,7 +119,7 @@ class SciELOLoader(BookLoader):
         publications = [["PDF", None, books_url, pdf_url], ["EPUB", eisbn, books_url, epub_url], ["PAPERBACK", isbn, books_url, None]]
         for publication_type, isbn, landing_page, full_text in publications:
             publication = {
-                "workId": work_id,
+                "workId": work.workId,
                 "publicationType": publication_type,
                 "isbn": isbn,
                 "widthMm": None,
@@ -152,19 +152,19 @@ class SciELOLoader(BookLoader):
             logging.info(f"created location: with publicationId {publication_id}")
             self.thoth.create_location(location)
 
-    def create_contributors(self, record, work, work_id):
+    def create_contributors(self, record, work):
         """Creates/updates all contributors associated with the current work and their contributions
 
         record: current JSON record
 
-        work_id: previously obtained ID of the current work
+        work: Work from Thoth
         """
         highest_contribution_ordinal = max((c.contributionOrdinal for c in work.contributions), default=0)
         for creator in record["creators"]:
             full_name_inverted = creator[1][1].split(',')
             name = full_name_inverted[1].strip()
             surname = full_name_inverted[0]
-            fullname = f"{name} {surname}"
+            full_name = f"{name} {surname}"
             contribution_type = self.contribution_types[creator[0][1]]
             orcid_id = None
             website = None
@@ -182,21 +182,32 @@ class SciELOLoader(BookLoader):
             contributor = {
                 "firstName": name,
                 "lastName": surname,
-                "fullName": fullname,
+                "fullName": full_name,
                 "orcid": orcid_id,
                 "website": website,
             }
-            if fullname not in self.all_contributors:
+            # determine the identifier to use (prefer ORCID ID if available)
+            identifier = orcid_id if orcid_id else full_name
+
+            # check if the contributor is not in Thoth
+            if identifier not in self.all_contributors:
+                # if not in Thoth, create a new contributor
                 contributor_id = self.thoth.create_contributor(contributor)
                 logging.info(f"created contributor: {contributor_id}")
-                self.all_contributors[fullname] = contributor_id
+                # cache new contributor
+                self.all_contributors[full_name] = contributor_id
+                if orcid_id:
+                    self.all_contributors[orcid_id] = contributor_id
             else:
-                contributor_id = self.all_contributors[contributor["fullName"]]
+                # if in Thoth, get the contributor_id and run
+                # update_scielo_contributor to check if any values need to be updated
+                contributor_id = self.all_contributors[identifier]
                 self.update_scielo_contributor(contributor, contributor_id)
+
             existing_contribution = next((c for c in work.contributions if c.contributor.contributorId == contributor_id), None)
             if not existing_contribution:
                 contribution = {
-                    "workId": work_id,
+                    "workId": work.workId,
                     "contributorId": contributor_id,
                     "contributionType": contribution_type,
                     "mainContribution": "true",
@@ -204,7 +215,7 @@ class SciELOLoader(BookLoader):
                     "biography": None,
                     "firstName": name,
                     "lastName": surname,
-                    "fullName": fullname,
+                    "fullName": full_name,
                 }
                 self.thoth.create_contribution(contribution)
                 logging.info(f"created contribution with contributorId: {contributor_id}")
@@ -247,15 +258,15 @@ class SciELOLoader(BookLoader):
                 self.thoth.update_contributor(combined_contributor)
                 logging.info(f"updated contributor: {contributor_id}")
         else:
-            logging.info(f"existing contributor: {contributor_id}")
+            logging.info(f"existing contributor, no changes needed to Thoth: {contributor_id}")
         return contributor
 
-    def create_languages(self, record, work, work_id):
+    def create_languages(self, record, work):
         """Creates language associated with the current work
 
         record: current JSON record
 
-        work_id: previously obtained ID of the current work
+        work: Work from Thoth
         """
         language_code = self.language_codes[record["language"]]
 
@@ -264,27 +275,27 @@ class SciELOLoader(BookLoader):
             logging.info("existing language")
             return
         language = {
-            "workId": work_id,
+            "workId": work.workId,
             "languageCode": language_code,
             "languageRelation": "ORIGINAL",
             "mainLanguage": "true"
         }
         self.thoth.create_language(language)
-        logging.info(f"created language for workId: {work_id}")
+        logging.info(f"created language for workId: {work.workId}")
 
-    def create_subjects(self, record, work, work_id):
+    def create_subjects(self, record, work):
         """Creates all subjects associated with the current work
 
         record: current JSON record
 
-        work_id: previously obtained ID of the current work
+        work: Work from Thoth
         """
         bisac_subject_code = record["bisac_code"][0][0][1]
         keyword_subject_codes = record["primary_descriptor"].split("; ")
 
         def create_subject(subject_type, subject_code, subject_ordinal):
             subject = {
-                "workId": work_id,
+                "workId": work.workId,
                 "subjectType": subject_type,
                 "subjectCode": subject_code,
                 "subjectOrdinal": subject_ordinal
