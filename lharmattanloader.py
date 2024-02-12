f79d957ad031e921fe1193c71b7a8b535a54ca7a
Brendan OConnell <boconnell1@gmail.com>
2024-02-12 12:24:21 +0100

    Completed most work on create_contributor

--- lharmattanloader.py
+++ lharmattanloader.py
@@ -40,6 +40,7 @@ class LHarmattanLoader(BookLoader):
                 work_id = self.thoth.create_work(work)
                 logging.info(f"created workId: {work_id}")
             work = self.thoth.work_by_id(work_id)
+            self.create_contributors(row, work)
 
 
     def get_work(self, row, imprint_id):
@@ -49,7 +50,6 @@ class LHarmattanLoader(BookLoader):
 
         imprint_id: previously obtained ID of this work's imprint
         """
-
 
         reference = row["uid"]
         doi = self.sanitise_doi(row["scs023_doi"])
@@ -114,3 +114,97 @@ class LHarmattanLoader(BookLoader):
             "pageInterval": None,
         }
         return work
+
+    def create_contributors(self, row, work):
+        """Creates/updates all contributors associated with the current work and their contributions
+
+        row: current CSV row
+
+        work: Work from Thoth
+        """
+
+        # we have authors, translators, contributors, and editors
+        # read data from each relevant field
+        authors = row["scs023_author"] if row["scs023_author"] else None
+        translators = row["scs023_translator"] if row["scs023_translator"] else None
+        contributors = row["contributor"] if row["contributor"] else None
+        editors = row["scs023_editor"] if row["scs023_editor"] else None
+        orcid = row["scs023_orcid"] if row["scs023_orcid"] else None
+        website = row["scs023_web"] if row["scs023_web"] else None
+
+        all_creators = [[authors, "AUTHOR"], [translators, "TRANSLATOR"], [contributors, "CONTRIBUTIONS_BY"], [editors, "EDITOR"]]
+
+
+        highest_contribution_ordinal = max((c.contributionOrdinal for c in work.contributions), default=0)
+        creator_category_count = 0
+        individual_creator_count = 0
+        for creators, contribution_type in all_creators:
+            if creators:
+                creator_category_count += 1
+                # names are separated by pipes, so use the pipe as a separator to put each set of names in an array
+                creators_array = creators.split("|")
+                # iterate over the elements of the array
+                for creator in creators_array:
+                    individual_creator_count += 1
+                    # sanitise names for Thoth formatting
+                    split_creator = creator.split()
+                    # names with middle initial, e.g. "K. Németh András"
+                    if len(split_creator) == 3:
+                        name = " ".join([split_creator[2], split_creator[0]])
+                        surname = split_creator[1]
+                        full_name = " ".join([split_creator[2], split_creator[0], split_creator[1]])
+                    elif len(split_creator) == 2:
+                        name = split_creator[1]
+                        surname = split_creator[0]
+                        full_name = " ".join(reversed(split_creator))
+                    else:
+                        name = surname = full_name = creator
+                    contributor = {
+                        "firstName": name,
+                        "lastName": surname,
+                        "fullName": full_name,
+                        "orcid": None,
+                        "website": None,
+                    }
+                    if full_name not in self.all_contributors:
+                        # if not in Thoth, create a new contributor
+                        contributor_id = self.thoth.create_contributor(contributor)
+                        logging.info(f"created contributor: {full_name}, {contributor_id}")
+                        # cache new contributor
+                        self.all_contributors[full_name] = contributor_id
+                    else:
+                        contributor_id = self.all_contributors[full_name]
+                        logging.info(f"contributor {full_name} already in Thoth, skipping")
+                    existing_contribution = next((c for c in work.contributions if c.contributor.contributorId == contributor_id), None)
+                    if not existing_contribution:
+                        contribution = {
+                            "workId": work.workId,
+                            "contributorId": contributor_id,
+                            "contributionType": contribution_type,
+                            "mainContribution": "true",
+                            "contributionOrdinal": highest_contribution_ordinal + 1,
+                            "biography": None,
+                            "firstName": name,
+                            "lastName": surname,
+                            "fullName": full_name,
+                        }
+                        self.thoth.create_contribution(contribution)
+                        logging.info(f"created contribution for {full_name}, type: {contribution_type} with contributorId: {contributor_id}")
+                        highest_contribution_ordinal += 1
+                    else:
+                        logging.info(f"existing contribution for {full_name}, type: {contribution_type} with contributorId: {contributor_id}")
+        if creator_category_count == 1 and individual_creator_count == 1:
+            logging.info(f"{full_name} is the only contributor for {work.title}")
+
+
+
+
+
+
+
+
+
+        # if only one creator, add orcid and website to them, else ignore
+        # add Contributor to Thoth attached by workId
+
+
